190ba5e712f0eb6a16fae3e40191b14ab10341c5
Joao Pedro Teixeira <joaopteixeira58@gmail.com>
2024-05-28 16:19:53 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,11 +23,12 @@ my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT
 ingredient_list = st.multiselect('Choose up to 5 ingredients:', my_dataframe, max_selections=5)
 
 if ingredient_list:
-    ingredients_string = ' '.join(ingredient_list)
-    #ingredients_string = ''
+    #ingredients_string = ' '.join(ingredient_list)
+    ingredients_string = ''
     for fruit_chosen in ingredients_string:
         ingredients_string += fruit_chosen + ' '
-        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+        st.subheader(fruit_chosen + ' Nutrition Information')
+        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_chosen)
         fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
 
     my_insert_stmt = f"""insert into smoothies.public.orders(ingredients, name_on_order) 
